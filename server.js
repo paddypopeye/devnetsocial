afe8b503525b7250cfcb6403984483411e1dd209
popeye <paddypopeye@hotmail.com>
2018-11-27 16:42:24 +0100

    Fixed error in heroku deployment

--- server.js
+++ server.js
@@ -43,5 +43,7 @@ app.get('*', (req,res) =>{
     res.sendFile(path.resolve(__dirname, 'mern-client', 'Build', 'index.html'));
     })//end get()
 }//end else
+
 const port = process.env.PORT || 5000
+
 app.listen(port, () => console.log(`Server running on ${port}`));
