deb8c198f457cd1241e95fef73921452a4870529
popeye <paddypopeye@hotmail.com>
2018-11-27 16:28:06 +0100

    Fixed error in server.js

--- server.js
+++ server.js
@@ -34,6 +34,6 @@ app.use('/api/posts', posts);
 
 //Static assets for production
 
-{(prrocess.env.NODE_ENV === 'production') ? app.use(express.static('mern-client/build')):app.get('*', (req,res) =>{res.sendFile(path.resolve(__dirname, 'mern-client', 'build', 'index.html'));})}//end get
+{(process.env.NODE_ENV === 'production') ? app.use(express.static('mern-client/build')):app.get('*', (req,res) =>{res.sendFile(path.resolve(__dirname, 'mern-client', 'build', 'index.html'));})}//end get
 const port = process.env.PORT || 5000
 app.listen(port, () => console.log(`Server running on ${port}`));
