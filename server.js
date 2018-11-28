469d4a14f2b8e6b2a654e8ed64deb448279f15ff
popeye <paddypopeye@hotmail.com>
2018-11-28 17:21:40 +0100

    changed server.js

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ if (process.env.NODE_ENV === 'production')
 else {
     
 app.get('*', (req,res) =>{
-    res.sendFile(path.resolve(__dirname, 'mern-client', 'Build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, 'mern-client', 'build', 'index.html'));
     })//end get() 
 }//end else
 
